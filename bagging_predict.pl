cd4a83cfb809e65f84b567db157908f6d041787b
id:ultraist <ultraist@private.udp.jp>
2009-08-15 12:36:43 +0900

    bagging:1.0 0.1

--- bagging_predict.pl
+++ bagging_predict.pl
@@ -20,7 +20,7 @@ $|=1;
 
 our @RECOMMENDER = (
 		    { file => "./results_base.txt",          weight => 1.0 },
-		    { file => "./results_base_author.txt",   weight => 0.2 }
+		    { file => "./results_base_author.txt",   weight => 0.1 }
 #		    { file => "./results_co_occurrence.txt", weight => 2.0 },
 #		    { file => "./results_author.txt",        weight => 1.6 },
 #		    { file => "./results_language.txt",      weight => 0.5 },
