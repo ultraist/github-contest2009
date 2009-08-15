62eef9b042af72dad5bddacde837bdbf9f4575a2
id:ultraist <ultraist@private.udp.jp>
2009-08-15 15:57:43 +0900

    bagging: param

--- bagging_predict.pl
+++ bagging_predict.pl
@@ -19,13 +19,13 @@ $|=1;
 # co_occurrence_predict 19.11  / 0.77637 = 24.61455
 
 our @RECOMMENDER = (
-		    { file => "./results_base.txt",          weight => 1.0 * 1.0 },
-		    { file => "./results_base_author.txt",   weight => 1.0 * 0.2 },
-		    { file => "./results_co_occurrence.txt", weight => 1.0 * 1.25 },
-		    { file => "./results_author.txt",        weight => 1.0 * 0.5 },
-		    { file => "./results_language.txt",      weight => 0.05 },
-		    { file => "./results_ranking.txt",       weight => 0.1 }
-#		    { file => "./results_fork.txt",          weight => 1.0 }
+		    { file => "./results_base.txt",          weight => 1.5 * 1.0 },
+		    { file => "./results_base_author.txt",   weight => 1.5 * 0.2 },
+		    { file => "./results_co_occurrence.txt", weight => 1.25 },
+		    { file => "./results_author.txt",        weight => 0.5 },
+		    { file => "./results_language.txt",      weight => 0.05  }
+#		    { file => "./results_ranking.txt",       weight => 0.005 }
+#		    { file => "./results_fork.txt",          weight => 0.05 }
 );
 
 sub rank_score
