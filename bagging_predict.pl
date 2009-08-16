3b566a60d1e7b8f24876d837cd6376ce10d0c941
id:ultraist <ultraist@private.udp.jp>
2009-08-16 10:16:43 +0900

    adjust bagging param

--- bagging_predict.pl
+++ bagging_predict.pl
@@ -19,11 +19,11 @@ $|=1;
 # co_occurrence_predict 19.11  / 0.77637 = 24.61455
 
 our @RECOMMENDER = (
-		    { file => "./results_base.txt",          weight => 3.0 },
+		    { file => "./results_base.txt",          weight => 4.0 },
 #		    { file => "./results_base_author.txt",   weight => 0.2 },
-		    { file => "./results_co_occurrence.txt", weight => 1.6 },
-		    { file => "./results_author.txt",        weight => 0.2 },
-		    { file => "./results_language.txt",      weight => 0.05  }
+		    { file => "./results_co_occurrence.txt", weight => 1.0 },
+		    { file => "./results_author.txt",        weight => 0.3 },
+		    { file => "./results_language.txt",      weight => 0.01  }
 #		    { file => "./results_ranking.txt",       weight => 0.005 }
 #		    { file => "./results_fork.txt",          weight => 0.05 }
 );
