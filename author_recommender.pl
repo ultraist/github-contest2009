c5fd33e84f4185ce3445730bf09c5c4ad9839a35
id:ultraist <ultraist@private.udp.jp>
2009-08-20 02:56:50 +0900

    author

--- author_recommender.pl
+++ author_recommender.pl
@@ -7,6 +7,39 @@ use Result;
 use Utils;
 
 $|=1;
+our $e = exp(1);
+sub sim
+{
+    my ($a, $h, $repo) = @_;
+    my $ok = 0;
+
+    foreach my $k (@$a) {
+	if (defined($h->{$k})) {
+	    $ok += 1;
+	}
+    }
+    if ($ok == 0) {
+	return 0.0;
+    }
+    my ($n1, $n2) = (scalar(@$a), scalar(keys(%$h)));
+    return $ok / ($n1 > $n2 ? $n1:$n2);
+}
+
+
+sub author_score
+{
+    my ($repo, $user_repos, $id) = @_;
+    my $max_sim = 0.0;
+    my $users = $repo->users($id);
+
+    foreach my $rid (@$user_repos) {
+	my $sim = sim($users, $repo->hash_users($rid));
+	if ($sim > $max_sim) {
+	    $max_sim = $sim;
+	}
+    }
+    return $max_sim + 0.01 * $repo->freq($id);
+}
 
 author_recommender:
 {
@@ -21,24 +54,27 @@ author_recommender:
     open(R, ">results_author.txt") or die $!;
     
     $repo->set_lang($lang);
+    $repo->set_users($user);
     $repo->ranking($user);
 
+
     foreach my $uid (@{$test->users()}) {
-	printf("$0: %.2f%%      \r", 100 * $i / $count);
+	printf("$0:                    %.2f%%      \r", 100 * $i / $count);
 	my @result_tmp;
 	my @result;
-	my $user_repos = $user->repos($uid);
-
-	foreach my $tid (@$user_repos) {
+	my @user_repos = @{$user->repos($uid)};
+	my @orign_user_repos = @user_repos;
+	foreach my $tid (@user_repos) {
 	    foreach my $rid (@{$repo->author_repos($tid)}) {
-		push(@result_tmp, { id => $rid, rank => $repo->rank($rid) });
+		printf("$0: $tid\r");
+		push(@result_tmp, { id => $rid, score => author_score($repo, \@orign_user_repos, $rid) });
 	    }
 	}
-	@result_tmp = sort { $a->{rank} <=> $b->{rank} } @result_tmp;
+	@result_tmp = sort { $b->{score} <=> $a->{score} } @result_tmp;
 	foreach my $rid (@result_tmp) {
-	    if (!Utils::includes($user_repos, $rid->{id})) {
+	    if (!Utils::includes(\@user_repos, $rid->{id})) {
 		push(@result, $rid->{id});
-		push(@$user_repos, $rid->{id});
+		push(@user_repos, $rid->{id});
 	    }
 	}
 	print R Result::format($uid, @result);
