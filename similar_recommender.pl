e0f18537ed8bb244302607ab7860e2ae70566114
id:ultraist <ultraist@private.udp.jp>
2009-08-19 22:09:31 +0900

    minor repos

--- similar_recommender.pl
+++ similar_recommender.pl
@@ -7,7 +7,7 @@ use Result;
 use Utils;
 use List::MoreUtils qw/uniq/;
 use constant {
-    K => 20
+    M => 1
 };
 $|=1;
 our $e = exp(1);
@@ -19,11 +19,11 @@ sub sim
 
     foreach my $k (@$a) {
 	if (defined($h->{$k})) {
-	    $ok += log(1.0 / scalar(@{$user->repos($k)}));
+	    $ok += 1.0;
 	}
     }
     my ($n1, $n2) = (scalar(@$a), scalar(keys(%$h)));
-    return $ok / ($n1 > $n2 ? $n1:$n2);
+    return $ok / ($n1 > $n2 ? $n2:$n1);
 }
 
 similar_recommender:
@@ -36,7 +36,6 @@ similar_recommender:
     my $test = new Result("./download/test.txt", $lang);
     my $count = $test->count();
     my $i = 0;
-    my $kfrac = 1.0 / K;
     
     open(R, ">results_similar.txt") or die $!;
     
@@ -50,15 +49,19 @@ similar_recommender:
 	my @result;
 	my @result_tmp;
 	my $user_repos = $user->repos($uid);
+	my @minor_repos;
+	my $c = 0;
 
 	foreach my $rid (@$user_repos) {
+	    push(@minor_repos, { id => $rid, rank => $repo->rank($rid) });
+	}
+	@minor_repos = sort { $b->{rank} <=> $a->{rank} } @minor_repos;
+
+	foreach my $rec (@minor_repos) {
+	    my $rid = $rec->{id};
 	    my $repos_user = $repo->users($rid);
 	    my @sim_repos;
 	    my @candidates;
-
-	    if ($repo->freq($rid) > 0.1) {
-		next;
-	    }
 
 	    foreach my $ruid (@$repos_user) {
 		push(@candidates, @{$user->repos($ruid)});
@@ -71,11 +74,15 @@ similar_recommender:
 		    push(@sim_repos, { id => $other_id, sim => $sim });
 		}
 	    }
-	    @sim_repos = sort { $b->{sim} <=> $a->{sim} } @sim_repos;
-	    if (@sim_repos > K) {
-		@sim_repos = @sim_repos[0 .. K - 1];
+	    if (scalar(@sim_repos) > 20) {
+		@sim_repos = sort { $b->{sim} <=> $a->{sim} } @sim_repos;
+		@sim_repos = @sim_repos[0 .. 19];
 	    }
 	    push(@result_tmp, @sim_repos);
+	    
+	    if (++$c >= M) {
+		last;
+	    }
 	}
 	
 	@result_tmp = sort { $b->{sim} <=> $a->{sim} } @result_tmp;
