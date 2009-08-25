c5d461e136d3895aac350bc0f26d1c51c07c6d4b
id:ultraist <ultraist@private.udp.jp>
2009-08-26 04:46:13 +0900

    param

--- ensemble_recommender.pl
+++ ensemble_recommender.pl
@@ -14,31 +14,31 @@ $|=1;
 our @RECOMMENDER = (
 		    {
 			file => "./results_forkbase.txt",
-			weight => 2.0,
+			weight => 1.0,
 			K => DEF_K,
 			score => sub { 1.0 / (1.5 + $_[0]); }
 		    },
 		    {
 			file => "./results_co_occurrence.txt",
-			weight => 1.5,
+			weight => 1.05,
 			K => DEF_K,
 			score => sub { 1.0 / (1.5 + $_[0]); }
 		    },
 		    {
 			file => "./results_author.txt",
-			weight => 0.5,
+			weight => 0.84,
 			K => DEF_K,
 			score => sub { 1.0 / (1.5 + $_[0]); }
 		    },
-		    {
-			file => "./results_name.txt",
-			weight => 0.06,
-			K => DEF_K,
-			score => sub { 1.0 / (1.5 + $_[0]); }
-		    },
+#		    {
+#			file => "./results_name.txt",
+#			weight => 0.06,
+#			K => DEF_K,
+#			score => sub { 1.0 / (1.5 + $_[0]); }
+#		    },
 		    {
 			file => "./results_popular.txt",
-			weight => 0.05,
+			weight => 0.4,
 			K => DEF_K,
 			score => sub { 1.0 / (1.5 + $_[0]); }
 		    }
