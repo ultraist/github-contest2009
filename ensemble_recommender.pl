9295c54c0ca732a7a7583ab1a962599c0311a30e
id:ultraist <ultraist@private.udp.jp>
2009-08-26 07:32:07 +0900

    param

--- ensemble_recommender.pl
+++ ensemble_recommender.pl
@@ -26,7 +26,7 @@ our @RECOMMENDER = (
 		    },
 		    {
 			file => "./results_author.txt",
-			weight => 0.5,
+			weight => 0.6,
 			K => DEF_K,
 			score => sub { 1.0 / (1.5 + $_[0]); }
 		    },
