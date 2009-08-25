01f91a86ae9f539c0f0df0417550251b2ca2d27d
id:ultraist <ultraist@private.udp.jp>
2009-08-26 07:17:28 +0900

    forkbase 0

--- conv_result.pl
+++ conv_result.pl
@@ -6,11 +6,21 @@ use Result;
 $ARGV[0] or die "usage: $0 file";
 
 my $result = new Result($ARGV[0]);
+my $n = $ARGV[1];
 
 open(R, ">results.txt") or die $!;
 foreach my $uid (sort {$a <=> $b } @{$result->users()}) {
     my $repos = $result->repos($uid);
-    print R Result::format($uid, Utils::padding_result(@$repos));
+    if (!defined($n)) {
+	print R Result::format($uid, Utils::padding_result(@$repos));
+    } else {
+	print R $uid, ":", join(",", split(//, 0 x ($n)));
+	if ($n != 0) {
+	    print R ",";
+	}
+	print R $repos->[$n];
+	print R join(",", split(//, 0 x (10-$n))),"\n";
+    }
 }
 close(R);
 
